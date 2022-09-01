972537235f96db7545761c4cafeb602a34254490
RafaelaEllensburg <71078526+RafaelaEllensburg@users.noreply.github.com>
2022-09-01 16:55:55 +0200

    Render phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,25 +2,21 @@ import React from "react";
 
 export default function Meaning(props) {
   console.log(props.meaning);
-  if (props.meaning) {
-    return (
-      <div className="Meaning">
-        <h3>{props.meaning.partOfSpeech}</h3>
-        {props.meaning.definitions.map(function (definition, index) {
-          return (
-            <div key={index}>
-              <p>
-                <strong>Definition: </strong> {definition.definition}
-                <br />
-                <strong>Example: </strong>
-                <em>{definition.example}</em>
-              </p>
-            </div>
-          );
-        })}
-      </div>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              <strong>Definition: </strong> {definition.definition}
+              <br />
+              <strong>Example: </strong>
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 import Synonyms from "./Synonyms";
 
 export default function Result(props) {
@@ -8,6 +9,13 @@ export default function Result(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetics, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetics={phonetics} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
