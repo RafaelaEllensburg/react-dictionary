1e998d815c20e1ee04230486d438a6cd78a93c0d
RafaelaEllensburg <71078526+RafaelaEllensburg@users.noreply.github.com>
2022-09-03 22:27:17 +0200

    Add definition strong to Meaning

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,15 +1,23 @@
 import React from "react";
 import "./Meaning.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definitions, index) {
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <div className="definition">{definitions.definition}</div>
-            <div className="example">{definitions.example}</div>
+            <div className="definition">
+              <strong>Definition: </strong>
+              <br />
+              {definition.definition}
+            </div>
+
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.definition.synonyms} />
           </div>
         );
       })}
